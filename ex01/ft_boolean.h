6334e80db47c846136097b4b28f871cc243b94ac
UnelDev <enee.prog@gmail.com>
2023-07-13 16:55:25 +0200

    fix norminette

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,21 +6,20 @@
 /*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 14:49:10 by edi-iori          #+#    #+#             */
-/*   Updated: 2023/07/13 16:17:32 by edi-iori         ###   ########lyon.fr   */
+/*   Updated: 2023/07/13 16:55:07 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-#ifndef		FT_BOOLEAN_H
-# define	FT_BOOLEAN_H
-
-# define TRUE		1
-# define FALSE		0
-# define SUCCESS	0
-typedef  int t_bool;
-# define EVEN(nb) (nb % 2)
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS '\n'
+# define EVEN(nb) (!(nb % 2))
 # define EVEN_MSG "I have an even number of arguments."
 # define ODD_MSG "I have an odd number of arguments."
+
+typedef int	t_bool;
 
 #endif
