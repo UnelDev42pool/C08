828a0ad496aa97c9994d3115fcb2217cd5af1d16
UnelDev <enee.prog@gmail.com>
2023-07-17 11:17:09 +0200

    add ex05

---
+++ ex05/ft_show_tab.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/14 14:16:31 by edi-iori          #+#    #+#             */
+/*   Updated: 2023/07/16 13:31:10 by edi-iori         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+// typedef struct s_stock_str
+// {
+// 	int		size;
+// 	char	*str;
+// 	char	*copy;
+// } t_stock_str;
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		nb = 147483648;
+		ft_putchar('-');
+		ft_putchar('2');
+	}
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+	}
+	if (nb >= 0 && nb < 10)
+	{
+		ft_putchar(nb + 48);
+	}
+	if (nb >= 10 && nb < 100)
+	{
+		ft_putchar((nb / 10) + 48);
+		ft_putchar((nb % 10) + 48);
+	}
+	if (nb > 100)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+void	ft_show_tab(t_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].size == 1 && par[i].str == 0)
+	{
+		write(1, par[i].str, ft_strlen(par[i].str));
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		write(1, par[i].copy, ft_strlen(par[i].copy));
+		write(1, "\n", 1);
+	}
+}
