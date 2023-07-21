987e555b69310f3d48b8b099c1a42da544d8156b
UnelDev <enee.prog@gmail.com>
2023-07-21 13:26:31 +0200

    fix bug in ex02

--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -6,11 +6,11 @@
 /*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 10:39:54 by edi-iori          #+#    #+#             */
-/*   Updated: 2023/07/21 13:22:50 by edi-iori         ###   ########lyon.fr   */
+/*   Updated: 2023/07/21 13:25:40 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
-# define ABS(value) (value * (value > 0) - (value < 0))
+# define ABS(value) (value * ((value > 0) - (value < 0)))
 #endif
